c9da53c60ac6f48072c4341c7ade8322048967a3
Ole Hannemann <ole.hannemann@stadtwerke-schwedt.de>
2022-10-26 18:10:49 +0200

    added documentation

--- py_auth_micro/WorkFlows/__init__.py
+++ py_auth_micro/WorkFlows/__init__.py
@@ -1,3 +1,22 @@
+"""This Package specifies classes which implement the most common workflows.
+
+following workflows are implemented:
+
+    * Sign In
+    * Sign Out
+    * Token Request
+    * Registration
+    * User creation (by Administrators)
+    * User deletion
+    * Group creation (by Administrators)
+    * Group deletion (by Administrators)
+    * Adding User to a Group (by Administrators)
+    * Removing User from a Group (by Administrators)
+
+All Function should return Dictionaries which can be easily parsed to JSON
+
+"""
+
 from ._userworkflow import UserWorkflow
 from ._sessionworkflow import SessionWorkflow
 from ._groupworkflow import GroupWorkflow
