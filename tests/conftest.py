72065ca675c8b79791bd830c3ffcfdfa247f4e70
Aurvandill <cerberus885@gmail.com>
2023-07-31 17:14:15 +0200

    started reworking tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,90 @@
 import pytest
-from tortoise.contrib.test import finalizer, initializer
+import asyncio
+import pytest_asyncio
+
+from tortoise import Tortoise
+from tortoise.contrib.test import getDBConfig
+from jwt_helper import JWTValidator, Issuer, SignMethod, JWTEncoder
+
+from py_auth_micro.Config import AppConfig
+from py_auth_micro.WorkFlows import GroupWorkflow
+
+DBURL = "sqlite://db.sqlite3"
 
 
-DBURL="sqlite://db.sqlite3"
-
-
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def initialize_tests(request):
-    initializer(["py_auth_micro.Models"], db_url=DBURL, app_label="models")
-    request.addfinalizer(finalizer)
+    config = getDBConfig(app_label="models", modules=["py_auth_micro.Models"])
+    loop = asyncio.get_event_loop()
+
+    async def _init_db() -> None:
+        await Tortoise.init(config)
+        await Tortoise.generate_schemas(safe=False)
+
+    async def _close_db() -> None:
+        await Tortoise.close_connections()
+
+    loop.run_until_complete(_init_db())
+
+    request.addfinalizer(lambda: loop.run_until_complete(_close_db()))
+
+
+@pytest.fixture
+def encoder():
+    return JWTEncoder("test_issuer", SignMethod.HS256, "123123")
+
+
+@pytest.fixture
+def access_token_admin(encoder):
+    return encoder.create_jwt(
+        {
+            "aud": ["test", "admin"],
+            "Type": "Access-Token",
+            "vhost": "test",
+        },
+        {"user": "test"},
+        3600,
+    )
+
+
+@pytest.fixture
+def access_token_non_admin(encoder):
+    return encoder.create_jwt(
+        {
+            "aud": ["test", "not_admin"],
+            "Type": "Access-Token",
+            "vhost": "test",
+        },
+        {"user": "test"},
+        3600,
+    )
+
+
+@pytest.fixture
+def id_token(encoder):
+    return encoder.create_jwt(
+        {
+            "kid": "1",
+            "aud": "127.0.0.1",
+            "vhost": "test",
+            "type": "ID-Token",
+        },
+        {"groups": ["test", "admin"], "username": "testuser"},
+        3600,
+    )
+
+
+@pytest_asyncio.fixture
+async def groupworkflow(initialize_tests):
+    return GroupWorkflow(
+        JWTValidator(
+            {"test_issuer": Issuer("test_issuer", "123123", [SignMethod.HS256])}
+        ),
+        AppConfig(),
+    )
+
+
+@pytest_asyncio.fixture
+async def test_data(initialize_tests, groupworkflow, access_token_admin):
+    await groupworkflow.create_group(access_token=access_token_admin, groupname="Test")
+    await groupworkflow.create_group(access_token=access_token_admin, groupname="Test2")
--- tests/groupworkflow_test.py
+++ tests/groupworkflow_test.py
@@ -1,104 +1,50 @@
 import pytest
-import importlib
-from pathlib import Path
-import sys
-from tortoise.contrib import test
-
-MODULE_PATH = f"{Path(__file__).parents[1]}/py_auth_micro/__init__.py"
-MODULE_NAME = "py_auth_micro"
-spec = importlib.util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
-module = importlib.util.module_from_spec(spec)
-sys.modules[spec.name] = module
-spec.loader.exec_module(module)
-
-from py_auth_micro.Config import AppConfig
-from py_auth_micro.WorkFlows import GroupWorkflow
-from jwt_helper import JWTValidator, Issuer, SignMethod, JWTEncoder
-
-GROUPWORKFLOW = GroupWorkflow(
-    JWTValidator({"test_issuer": Issuer("test_issuer", "123123", [SignMethod.HS256])}),
-    AppConfig(),
-)
-ENCODER = JWTEncoder("test_issuer", SignMethod.HS256, "123123")
-
-ACCESS_TOKEN_ADMIN = ENCODER.create_jwt(
-    {
-        "aud": ["test", "admin"],
-        "Type": "Access-Token",
-        "vhost": "test",
-    },
-    {"user": "test"},
-    3600,
-)
-
-ACCESS_TOKEN_NON_ADMIN = ENCODER.create_jwt(
-    {
-        "aud": ["test", "not_admin"],
-        "Type": "Access-Token",
-        "vhost": "test",
-    },
-    {"user": "test"},
-    3600,
-)
-
-ID_TOKEN = ENCODER.create_jwt(
-    {
-        "kid": "1",
-        "aud": "127.0.0.1",
-        "vhost": "test",
-        "type": "ID-Token",
-    },
-    {"groups": ["test", "admin"], "username": "testuser"},
-    3600,
-)
+import pytest_asyncio
 
 
-class TestGroupWorkflow(test.TestCase):
-    @pytest.mark.asyncio
-    async def test_create_group(self):
-        resp = await GROUPWORKFLOW.create_group(
-            access_token=ACCESS_TOKEN_ADMIN, groupname="Test2"
+
+
+@pytest.mark.asyncio
+class TestGroupWorkflow:
+
+    async def test_create_group(self, initialize_tests, groupworkflow, access_token_admin):
+        resp = await groupworkflow.create_group(
+            access_token=access_token_admin, groupname="Test2"
         )
         assert resp["resp_code"] == 200
 
-    async def test_create_group_invalid_group(self):
-        resp = await GROUPWORKFLOW.create_group(
-            access_token=ACCESS_TOKEN_ADMIN, groupname="testasdasdasd asdasd____ asdasd"
+    async def test_create_group_invalid_group(self, initialize_tests, groupworkflow, access_token_admin):
+        resp = await groupworkflow.create_group(
+            access_token=access_token_admin, groupname="testasdasdasd asdasd____ asdasd"
         )
         assert resp["resp_code"] == 500
 
-    async def test_create_group_double(self):
+    async def test_create_group_double(self, initialize_tests, groupworkflow, access_token_admin):
         with pytest.raises(ValueError):
-            await GROUPWORKFLOW.create_group(
-                access_token=ACCESS_TOKEN_ADMIN, groupname="test"
+            await groupworkflow.create_group(
+                access_token=access_token_admin, groupname="test"
             )
-            await GROUPWORKFLOW.create_group(
-                access_token=ACCESS_TOKEN_ADMIN, groupname="test"
+            await groupworkflow.create_group(
+                access_token=access_token_admin, groupname="test"
             )
 
-    async def test_create_group_double_capitalisation(self):
-        await GROUPWORKFLOW.create_group(
-            access_token=ACCESS_TOKEN_ADMIN, groupname="test"
+    async def test_create_group_double_capitalisation(self, initialize_tests, groupworkflow, access_token_admin):
+        await groupworkflow.create_group(
+            access_token=access_token_admin, groupname="test"
         )
-        resp = await GROUPWORKFLOW.create_group(
-            access_token=ACCESS_TOKEN_ADMIN, groupname="Test"
+        resp = await groupworkflow.create_group(
+            access_token=access_token_admin, groupname="Test"
         )
         assert resp["resp_code"] == 200
 
-    async def test_get_groups_empty(self):
+    async def test_get_groups_empty(self, initialize_tests, groupworkflow, access_token_non_admin):
         # print(str(await GROUPWORKFLOW.get_groups(access_token=blub)))
-        resp = await GROUPWORKFLOW.get_groups(access_token=ACCESS_TOKEN_NON_ADMIN)
+        resp = await groupworkflow.get_groups(access_token=access_token_non_admin)
         assert resp["resp_code"] == 204
         assert resp["resp_data"]["groups"] == []
 
-    async def test_get_groups(self):
-        # print(str(await GROUPWORKFLOW.get_groups(access_token=blub)))
-        await GROUPWORKFLOW.create_group(
-            access_token=ACCESS_TOKEN_ADMIN, groupname="Test"
-        )
-        await GROUPWORKFLOW.create_group(
-            access_token=ACCESS_TOKEN_ADMIN, groupname="Test2"
-        )
-        resp = await GROUPWORKFLOW.get_groups(access_token=ACCESS_TOKEN_NON_ADMIN)
+    async def test_get_groups(self, test_data, groupworkflow, access_token_non_admin):
+        print(groupworkflow)
+        resp = await groupworkflow.get_groups(access_token=access_token_non_admin)
         assert resp["resp_code"] == 200
         assert resp["resp_data"]["groups"] == ["Test","Test2"]
