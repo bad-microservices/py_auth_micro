015d88e4041cd7662c5ff0b080660089f7cbdba5
Ole Hannemann <ole.hannemann@stadtwerke-schwedt.de>
2024-08-27 10:31:48 +0200

    see cahngelog

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
-with open('py_auth_micro/_version.py') as infile:
+
+with open("py_auth_micro/_version.py") as infile:
     version_line = infile.read()
 
-version_line = version_line.replace("__version__","")
-version_line = version_line.replace("=","")
-version_line = version_line.replace(" ","")
-version_line = version_line.replace(" ","")
-version = version_line.replace("\"","")
-setup(
-
-    version=version
+version_line = (
+    version_line.replace("__version__", "")
+    .replace("\n", "")
+    .replace("=", "")
+    .replace(" ", "")
+    .replace(" ", "")
 )
+version = version_line.replace('"', "")
+setup(version=version)
